53207b794016c1e438ea17349fb756e47d9c6106
Aum <candevfol@gmail.com>
2025-09-21 12:29:57 +0530

    express server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from 'dotenv'
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT
+
+app.get('/',(req,res)=>{
+    res.send('working fine');
+})
+
+app.listen(PORT, ()=>{
+    console.log(`App is listning to: ${PORT}`)
+})
